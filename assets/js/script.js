d133c9df0d6bb3e22605cdbbf2707d9fdfb99919
Justus marwa <justusmwita@gmail.com>
2021-08-22 18:48:18 +0000

    minir changes on index page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,39 @@
-function.rungame(){
+//wait for the DOM to finish before running the code
+//get the button elements and event listeners to them
+document.addEventListener("DomContentLoaded",funtion()){
    
+}
+let buttons=document.getElementByTAgName("button");
+for (let button of buttons){
+   
+}
+
+   function runGame() {
+   
+       // Creates two random numbers between 1 and 25
+       let swedOne=["hi","come", "go", "eat", "drink"," help", "welcome",  "goodbye", "morning ","afternoon","evening","night" ];
+       let engOne=["hej","kom","gå","ät","dryck","hjälp","välkommen","hejdå","morgon","eftermiddag","kväll","natt" ];
    }
-   function.checkAnswer(){
+   function checkAnswer(){
+   if (swedOne==engOne){ return[ "correct answer"];
+
+}
+else{ alert[ "incorrect answer"];
+   
+};
+}
+function calculateCorrectAnswer(){
    
 }
-function.calculateCorrectAnswer(){
-   
+function incrementScore(){
 }
-function.incrementScore(){
+function incrementWrongAnswer(){
 }
-function.incrementWrongAnswer(){
+function displayBeginnerQuestions(){
 }
-function.displayBeginnerQuestions(){
+function displaywordAssociation(){
 }
-function.displaywordAssociation(){
-}
-function.displayFillInBlanks(){
+function displayFillInBlanks(){
 }
 
 
