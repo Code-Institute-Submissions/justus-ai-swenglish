5693d7e5d58c813f4e365fd378ec29232bfb74e3
Justus marwa <justusmwita@gmail.com>
2021-08-23 09:16:23 +0000

    gethe swedish indices to march the english indices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,48 @@
 //wait for the DOM to finish before running the code
 //get the button elements and event listeners to them
-document.addEventListener("DomContentLoaded",funtion()){
-   
-}
-let buttons=document.getElementByTAgName("button");
-for (let button of buttons){
-   
-}
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
    function runGame() {
    
-       // Creates two random numbers between 1 and 25
-       let swedOne=["hi","come", "go", "eat", "drink"," help", "welcome",  "goodbye", "morning ","afternoon","evening","night" ];
-       let engOne=["hej","kom","gå","ät","dryck","hjälp","välkommen","hejdå","morgon","eftermiddag","kväll","natt" ];
+       // Creates two paralell arrays to compare between swedish and english words for the basic level
+       let engOne=["hi","come", "go", "eat", "drink"," help", "welcome",  "goodbye", "morning ","afternoon","evening","night" ];
+       let swedOne=["hej","kom","gå","ät","dryck","hjälp","välkommen","hejdå","morgon","eftermiddag","kväll","natt" ];
    }
    function checkAnswer(){
-   if (swedOne==engOne){ return[ "correct answer"];
+   if (swedOne.indexOf("hej")===engOne.indexOf("hi")){ return[ "correct answer"];
 
 }
 else{ alert[ "incorrect answer"];
    
 };
 }
+/**
+ * Gets the operands (the swedish indices) and the level operator (basic,details, fill in etc)
+ * to return the correct answer.
+ */
 function calculateCorrectAnswer(){
+    let userAnswer = engOne(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Great!");
+        incrementScore();
+    } else {
+        alert(`try again${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
+    }
    
 }
 function incrementScore(){
@@ -31,8 +51,7 @@ function incrementWrongAnswer(){
 }
 function displayBeginnerQuestions(){
 }
-function displaywordAssociation(){
-}
+
 function displayFillInBlanks(){
 }
 
