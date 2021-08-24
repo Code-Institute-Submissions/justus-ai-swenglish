b98acee0f2754749d5f5b115278445b064291e29
Justus marwa <justusmwita@gmail.com>
2021-08-24 15:28:17 +0000

    js start game console message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,37 @@
-//wait for the DOM to finish before running the code
-//get the button elements and event listeners to them
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+//starting game from the beginner level
+ 
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
-            }
-        });
+
+function runGame() {  }
+let beginnerButton=document.getElementsByClassName("btn btn--big btn--green")
+let questionarea=document.getElementById("operand1")
+
+beginnerButton=(addEventListener)("click", startGame)
+
+function startGame() {  
+console.log("started")
+}
+// Creates two paralell arrays to compare between swedish and english words for the basic level
+let engOne = ["hi", "come", "go", "eat", "drink", " help", "welcome", "goodbye", "morning ", "afternoon", "evening", "night"];
+let swedOne = ["hej", "kom", "gå", "ät", "dryck", "hjälp", "välkommen", "hejdå", "morgon", "eftermiddag", "kväll", "natt"];
+
+
+function checkAnswer() {
+    if (swedOne.indexOf("hej") === engOne.indexOf("hi")) {
+        alert("Correct answer");
+        incrementScore()
     }
-   function runGame() {
-   
-       // Creates two paralell arrays to compare between swedish and english words for the basic level
-       let engOne=["hi","come", "go", "eat", "drink"," help", "welcome",  "goodbye", "morning ","afternoon","evening","night" ];
-       let swedOne=["hej","kom","gå","ät","dryck","hjälp","välkommen","hejdå","morgon","eftermiddag","kväll","natt" ];
-   }
-   function checkAnswer(){
-   if (swedOne.indexOf("hej")===engOne.indexOf("hi")){ return[ "correct answer"];
+    else {
+        alert("Incorrect answer");
+        incrementWrongAnswer();
+    };
+}
 
-}
-else{ alert[ "incorrect answer"];
-   
-};
-}
 /**
  * Gets the operands (the swedish indices) and the level operator (basic,details, fill in etc)
  * to return the correct answer.
  */
-function calculateCorrectAnswer(){
+function calculateCorrectAnswer() {
     let userAnswer = engOne(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
@@ -39,20 +39,33 @@ function calculateCorrectAnswer(){
     if (isCorrect) {
         alert("Great!");
         incrementScore();
-    } else {
-        alert(`try again${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
-        incrementWrongAnswer();
+
     }
-   
-}
-function incrementScore(){
-}
+    function incrementScore() {
+        let userAnswer = engOne(document.getElementById("answer-box").value);
+        let calculatedAnswer = calculateCorrectAnswer();
+        let isCorrect = userAnswer === calculatedAnswer[0];
+
+        if (isCorrect) {
+            alert("Great!");
+            incrementScore();
+        } else {
+            alert(`try again${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+            incrementWrongAnswer();
+        }
+    }
+    function incrementWrongAnswer() {
+    }
 function incrementWrongAnswer(){
 }
-function displayBeginnerQuestions(){
+
+    function displayMoreQuestions() {
+    }
 }
-
-function displayFillInBlanks(){
+// start game
+function init() {
+    // call fucntions here that will start your game
 }
-
-
+init()
+    
+    
