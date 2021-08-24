2f2f37cc5794c3ada12c72e791693ac705012a6c
Justus marwa <justusmwita@gmail.com>
2021-08-24 22:11:09 +0000

    styling of the buttons and the background in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//starting game from the beginner level
+//starting game 
  
 
 
@@ -16,56 +16,3 @@ let engOne = ["hi", "come", "go", "eat", "drink", " help", "welcome", "goodbye",
 let swedOne = ["hej", "kom", "gå", "ät", "dryck", "hjälp", "välkommen", "hejdå", "morgon", "eftermiddag", "kväll", "natt"];
 
 
-function checkAnswer() {
-    if (swedOne.indexOf("hej") === engOne.indexOf("hi")) {
-        alert("Correct answer");
-        incrementScore()
-    }
-    else {
-        alert("Incorrect answer");
-        incrementWrongAnswer();
-    };
-}
-
-/**
- * Gets the operands (the swedish indices) and the level operator (basic,details, fill in etc)
- * to return the correct answer.
- */
-function calculateCorrectAnswer() {
-    let userAnswer = engOne(document.getElementById("answer-box").value);
-    let calculatedAnswer = calculateCorrectAnswer();
-    let isCorrect = userAnswer === calculatedAnswer[0];
-
-    if (isCorrect) {
-        alert("Great!");
-        incrementScore();
-
-    }
-    function incrementScore() {
-        let userAnswer = engOne(document.getElementById("answer-box").value);
-        let calculatedAnswer = calculateCorrectAnswer();
-        let isCorrect = userAnswer === calculatedAnswer[0];
-
-        if (isCorrect) {
-            alert("Great!");
-            incrementScore();
-        } else {
-            alert(`try again${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
-            incrementWrongAnswer();
-        }
-    }
-    function incrementWrongAnswer() {
-    }
-function incrementWrongAnswer(){
-}
-
-    function displayMoreQuestions() {
-    }
-}
-// start game
-function init() {
-    // call fucntions here that will start your game
-}
-init()
-    
-    
