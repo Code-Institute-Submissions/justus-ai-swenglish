ce1522de049275421e7f732df28ccac6d09ed550
Justus marwa <justusmwita@gmail.com>
2021-11-26 12:52:56 +0000

    added a restart buttonand a home button to the game page

--- assets/js/end.js
+++ assets/js/end.js
@@ -20,11 +20,11 @@ saveHighScore = e => {
         name: username.value
     }
     highScores.push(score)  
-    highScores.sort((a,b) = {
+    highScores.sort(function(a,b) {
         return b.score -a.score
     } )
     highScores.splice(5)
     
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign(highscores.html)
+    window.location.assign("highscores.html")
 }
