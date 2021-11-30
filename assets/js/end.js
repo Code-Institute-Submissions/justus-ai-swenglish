6e8298a56fb64e8e232171e76c5fef31e3088eac
Justus marwa <justusmwita@gmail.com>
2021-11-30 11:50:20 +0000

    Add new screenshots to the revsed verson of the game on the readme file

--- assets/js/end.js
+++ assets/js/end.js
@@ -8,20 +8,20 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 const MAX_HIGH_SCORES = 5;
 let saveHighScore;
-​
+
 // update final score
 finalScore.innerText = mostRecentScore;
-​
+
 // enable save button if username exists
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
-​
+
 // save the highscore once username exists
 saveScoreBtn.addEventListener('click', function () {
     saveHighScore(event);
 });
-​
+
 // save localStorage highscore, and direct user to leaderboard
 saveHighScore = e => {
     e.preventDefault();
@@ -34,7 +34,7 @@ saveHighScore = e => {
         return b.score - a.score;
     });
     highScores.splice(MAX_HIGH_SCORES);
-​
+
     localStorage.setItem('highScores', JSON.stringify(highScores));
     window.location.href = 'highscores.html';
 };
--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,12 +1,12 @@
 /* jshint esversion: 8*/
-​
+
 // global variables
 const highscoresList = document.querySelector('#highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-​
+
 // insert localStorage scores into leaderboard
 highscoresList.innerHTML =
     highScores.map((score, index) => {
         return `<li classs="high-score">${index + 1}. ${score.name} - ${score.score} pts</li>`;
-​
+
     }).join('');
